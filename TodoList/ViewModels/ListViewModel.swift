ba333d6f499ed8a492061d0e7afc61ad72fdf559
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2024-08-04 23:44:34 +0530

    User Experience and Animations in SwiftUI app | Todo List #5 22:31   14:34 / 14:40  Save and persist data with UserDefaults

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -19,20 +19,32 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init(){
         getItems()
     }
     
     func getItems() {
+
+//    let newItems = [
+//        ItemModel(title: "This is the first title", isCompleted: false),
+//        ItemModel(title: "This is second", isCompleted: true),
+//        ItemModel(title: "This is the third title", isCompleted: false)
+//    ]
+//    items.append(contentsOf: newItems)
+      
+        guard 
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {return}
         
-    let newItems = [
-        ItemModel(title: "This is the first title", isCompleted: false),
-        ItemModel(title: "This is second", isCompleted: true),
-        ItemModel(title: "This is the third title", isCompleted: false)
-    ]
-        items.append(contentsOf: newItems)
+        self.items = savedItems
     }
     
     func deleteItem(indexSet: IndexSet) {
@@ -51,6 +63,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
