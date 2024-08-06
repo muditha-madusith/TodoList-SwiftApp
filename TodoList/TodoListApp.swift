fc26b4464538804d408cecc38e600dccd21e00cd
Muditha Madusith <108977790+muditha-madusith@users.noreply.github.com>
2024-08-07 00:40:12 +0530

    Update landscape mode

--- TodoList/TodoListApp.swift
+++ TodoList/TodoListApp.swift
@@ -25,6 +25,7 @@ struct TodoListApp: App {
             NavigationView{
                 ListView()
             }
+            .navigationViewStyle(StackNavigationViewStyle())
             .environmentObject(listViewModel)
         }
     }
